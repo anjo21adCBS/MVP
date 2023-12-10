be44e667f04a24713f8087a7da30031035b7f4ed
anjo21ad <skjoldemosejohansen@gmail.com>
2023-12-10 10:01:59 +0100

    Opdateret navigationsfil

--- App.js
+++ App.js
@@ -1,18 +1,6 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import View1 from '.screens/View1';
-import View2 from '.screens/View2';
-
-const Stack = createStackNavigator();
+import Navigation from './navigation/navigation';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="View1">
-        <Stack.Screen name="View1" component={View1} />
-        <Stack.Screen name="View2" component={View2} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Navigation />;
 }
